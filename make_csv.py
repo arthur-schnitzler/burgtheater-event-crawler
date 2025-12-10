6e79bd6b4a2a1a8ccb80b911ad61660df32bbf35
csae8092 <p.andorfer@gmail.com>
2025-12-10 10:04:13 +0100

    feat: events as csv

--- make_csv.py
+++ make_csv.py
@@ -3,10 +3,10 @@ import os
 
 import pandas as pd
 
-from utils import data
+from utils import data as data_folder
 
-input_json = os.path.join(data, "persons.json")
-output_csv = os.path.join(data, "persons.csv")
+input_json = os.path.join(data_folder, "persons.json")
+output_csv = os.path.join(data_folder, "persons.csv")
 
 with open(input_json, "r", encoding="utf-8") as fp:
     data = json.load(fp)
@@ -18,8 +18,8 @@ df["occupation"] = "Schauspieler"
 df.to_csv(output_csv, index=False)
 
 
-input_json = os.path.join(data, "events.json")
-output_csv = os.path.join(data, "events.csv")
+input_json = os.path.join(data_folder, "events.json")
+output_csv = os.path.join(data_folder, "events.csv")
 
 with open(input_json, "r", encoding="utf-8") as fp:
     data = json.load(fp)
@@ -27,4 +27,5 @@ with open(input_json, "r", encoding="utf-8") as fp:
 items = data.values()
 
 df = pd.DataFrame(items)
+df = df.sort_values("date", ascending=True)
 df.to_csv(output_csv, index=False)
